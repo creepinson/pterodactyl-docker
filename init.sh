d8b721a3d555d26142a94c0df5aa557674f3dcbe
Theo Paris <theoparisdesigns@gmail.com>
2021-01-28 10:58:23 -0800

    fix no interaction on p:environment:setup

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ echo Initializing panel at "$panel_url"
 cp .env.example data/panel/.env
 # Run environment setup
 docker-compose run --rm php php artisan key:generate --force --no-interaction
-docker-compose run --rm php php artisan p:environment:setup --new-salt --author="admin@example.com" --url="$panel_url" --timezone="America/Los_Angeles"
+docker-compose run --rm php php artisan p:environment:setup --new-salt --author="admin@example.com" --url="$panel_url" --timezone="America/Los_Angeles" --no-interaction
 docker-compose run --rm php php artisan migrate
 docker-compose run --rm php php artisan db:seed
 docker-compose run --rm php php artisan p:user:make --email=admin@example.com --username=admin --name-first=Admin --name-last=User --password=12Secret34 --admin=1
